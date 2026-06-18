802574a3b6c4e3849b8754dbfde42e6709977162
Kristel C <kristelc12@example.com>
2026-06-18 06:11:43 -0600

    config gh p

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/proyecto_personal_c22355/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
